a81fe3ff2787b4906e54f917a6d6b4aa6ca339ef
Nor-Eddin <noreddinlam@gmail.com>
2023-04-14 13:44:14 +0200

    ajout des projet Application et interface

---
+++ suivi-des-drones.Core.Application/Repositories/DroneRepository.cs
@@ -0,0 +1,27 @@
+﻿using suivi_des_drones.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suivi_des_drones.Core.Application.Repositories
+{
+    /// <summary>
+    /// Repository qui gére le CRUD
+    /// </summary>
+    /// <returns></returns>
+    public class DroneRepository
+    {
+        /// <summary>
+        /// Return la liste compléte des drones
+        /// </summary>
+        /// <returns></returns>
+        #region Public Methode
+        public List<Drone> GetAll()
+        {
+            return new ();
+        }
+        #endregion
+    }
+}
---
+++ suivi-des-drones.Core.Models/HealthStatus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suivi_des_drones.Core.Models
+{
+ 
+    //Facon avec enum
+    //public enum HealthStatus
+    //{
+    //    ok=0,
+    //    broken=-1,
+    //    repair=-2
+    //}
+
+    //Facon class
+      //public class HealthStatus
+      //{
+      //    public static HealthStatus OK = new HealthStatus() { Id = 0, Label = "OK" };
+      //    public static HealthStatus Broken = new HealthStatus() { Id = -1, Label = "Cassé" };
+      //    public static HealthStatus Repair = new HealthStatus() { Id = -2, Label = "En réparation" };
+      //    public int Id { get; set; } = 0;
+      //    public string Label { get; set; } = default!;
+      //}
+
+    //Facon record en 2 exemples
+
+    //public record HearthStatus(int Id,string Label);
+
+    public record HealthStatus
+    {
+        public static HealthStatus OK = new HealthStatus() { Id = 0, Label = "OK" };
+        public static HealthStatus Broken = new HealthStatus() { Id = -1, Label = "Cassé" };
+        public static HealthStatus Repair = new HealthStatus() { Id = -2, Label = "En réparation" };
+        public int Id { get; init; }
+        public string Label { get; init; } = default!;
+
+    }
+
+}
