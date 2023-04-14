3ced14c3e9d69bd12db6004bde0c269aa87db667
Nor-Eddin <noreddinlam@gmail.com>
2023-04-14 12:56:51 +0200

    Ajoutez des fichiers projet.

---
+++ suivi-des-drones.Core.Models/Drone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suivi_des_drones.Core.Models
+{
+    /// <summary>
+    /// Drones de l'application
+    /// </summary>
+    public class Drone
+    {
+        #region Properties
+
+        #endregion
+    }
+}
