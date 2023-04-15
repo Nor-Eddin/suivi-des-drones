ddd3cc6cb07db60ed863f7c9812fc67c234bc3d6
Nor-Eddin <noreddinlam@gmail.com>
2023-04-15 14:43:49 +0200

    Ajout de entity framework et du DBContext

--- suivi-des-drones.Web.UI/Pages/Index.cshtml.cs
+++ suivi-des-drones.Web.UI/Pages/Index.cshtml.cs
@@ -6,16 +6,56 @@ namespace suivi_des_drones.Web.UI.Pages
 {
     public class IndexModel : PageModel
     {
+        #region Fields
         private readonly ILogger<IndexModel> _logger;
-
+        #endregion
+        #region constructor
         public IndexModel(ILogger<IndexModel> logger)
         {
+           
             _logger = logger;
         }
 
-        public void OnGet()
+        //public void OnGet()
+        //{
+        //    var drone = new Drone();
+        //}
+
+        public IActionResult OnGet()
         {
-            var drone = new Drone();
+            IActionResult result=this.Page();
+                 this.SetListOfDrones();
+                 this.SetListOfStatus();
+            return result;
         }
+
+        //public async Task<IActionResult> OnGetAsync()
+        //{
+        //    this.SetListOfDrones();
+        //    this.SetListOfStatus();
+        //    return this.Page();
+        //}
+        #endregion
+        #region Public methods
+
+        #endregion
+        #region Internal Methods
+        private void SetListOfDrones()
+        {
+
+        this.Drones.Add(new () { Matricule = "54XXD0", CreationDate = DateTime.Now});
+        this.Drones.Add(new () { Matricule = "15FDR14", CreationDate = DateTime.Now.AddDays(-180) });
+        }
+        private void SetListOfStatus()
+        {
+            this.StatusList.Add(HealthStatus.OK);
+            this.StatusList.Add(HealthStatus.Broken);
+            this.StatusList.Add(HealthStatus.Repair);
+        }
+        #endregion
+        #region Properties
+        public List<Drone> Drones { get; set; } = new();
+        public List<HealthStatus>StatusList { get; set; } = new();
+        #endregion
     }
 }
